a560506bc993db05cb2f7dbf01436eb9623a7667
Joaquin <joaquinserra94@gmail.com>
2024-04-24 07:10:46 +0200

    Actividad Nº1 Automatas

--- Uc1_AFN.py
+++ Uc1_AFN.py
@@ -34,7 +34,7 @@ class AFN:
                 return False                                                #Retorna False si no se alcanza un estado.
         return estado_actual == self.estado_aceptado                        #Retorna el estado actual si es el estado aceptado.
     
-afn = AFN()                                                                 #Instancia de la clase AFN.
+'''afn = AFN()                                                                 #Instancia de la clase AFN.
 
 strings_aceptados = ["usuario@ejemplo.com", "nombre.apellido@ejemplo.es"]   #Realizamos una lista de strings que deberian ser aceptados.
 for strings in strings_aceptados:
@@ -51,4 +51,20 @@ for strings in strings_no_aceptados:
         print(f"'{strings}' es una direccion de correo válida!!")
     else:                                                                   
         print(f"'{strings}' no es una direccion de correo válida!!")
+        print("Por favor, intenta ingresar un correo válido.")'''
+
+def inicio():
+    print("Bienvenido al validador de direcciones de correo electrónico de la actividad nº1 de Teoría de Autómatas y Lenguajes Formales.")
+    print("Por favor, introduce una dirección de correo electrónico asi puedo verificar si es valida o no.")
+    email = input("Dirección de correo electrónico: ")                      #Solicitamos al usuario que introduzca una dirección de correo.
+    afn = AFN()                                                             #Instancia de la clase AFN.
+    if afn.proceso_input(email):                                            #Si el string es aceptado, se notifica al usuario con un mensaje.
+        print(f"'{email}' es una direccion de correo válida!!")
+        print("Gracias por utilizar el validador de direcciones de correo electrónico de la actividad nº1 de Teoría de Autómatas y Lenguajes Formales.")
+        inicio()                                                            #Llamamos a la función inicio para iniciar el programa.
+    else:                                                                   #Si el string no es aceptado, se notifica al usuario con un mensaje.
+        print(f"'{email}' no es una direccion de correo válida!!")
         print("Por favor, intenta ingresar un correo válido.")
+        inicio()                                                                    #Llamamos a la función inicio para iniciar el programa.
+        #email()
+inicio()  
