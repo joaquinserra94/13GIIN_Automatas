d70df86c06bf29e41f59b76e1db653a5cf6d3daf
Joaquin <joaquinserra94@gmail.com>
2024-04-25 07:22:24 +0200

    Actividad Nº1 Automatas

--- Uc1_AFN.py
+++ Uc1_AFN.py
@@ -10,7 +10,7 @@
 #Validación de direcciones de correo electrónico.
 class AFN:
     def __init__(self):                                                     #Implementacion de diccionarios para representar los
-        self.trancisiones = {                                               #estados y transiciones del AFN.
+        self.transiciones = {                                               #estados y transiciones del AFN.
             "q0": {"char": ["q1"]},                                         #Estado inicial q0 a q1, donde se identificara con un caracter.
             "q1": {"char": ["q1"], "@": ["q2"]},                            #Estado q1 a q2, donde se identificará el limite con un @.
             "q2": {"char": ["q2"], ".": ["q3"]},                            #Estado q2 a q3, donde se identificará el limite con un punto.
@@ -24,11 +24,11 @@ class AFN:
             estados = []                                                    #Lista para almacenar estados alcanzados.
             #print(estado_actual)
             #print(estados)
-            if char in self.trancisiones[estado_actual]:
-                estados += self.trancisiones[estado_actual][char]           #Agrega los estados alcanzados a la lista estados.
+            if char in self.transiciones[estado_actual]:
+                estados += self.transiciones[estado_actual][char]           #Agrega los estados alcanzados a la lista estados.
                 #print(estados)
-            if "char" in self.trancisiones[estado_actual]:
-                estados += self.trancisiones[estado_actual]["char"]         #Agrega los estados alcanzados a la lista estados.
+            if "char" in self.transiciones[estado_actual]:
+                estados += self.transiciones[estado_actual]["char"]         #Agrega los estados alcanzados a la lista estados.
                 #print(estados)
             estado_actual = estados[0] if estados else None                 #Actualiza el estado actual.
             #print(estado_actual)
@@ -53,14 +53,14 @@ def inicio():
 inicio()  
 
 '''afn = AFN()                                                              #Instancia de la clase AFN.
-'''
+
 strings_aceptados = ["usuario@ejemplo.com", "nombre.apellido@ejemplo.es"]   #Realizamos una lista de strings que deberian ser aceptados.
 for strings in strings_aceptados:
     if afn.proceso_input(strings):                                          #Si el string es aceptado, se notifica al usuario con un mensaje.
         print(f"'{strings}' es una direccion de correo válida!!")
     else:                                                                   #Si el string no es aceptado, se notifica al usuario con un mensaje.
         print(f"'{strings}' no es una direccion de correo válida!!")
-        
+        print("Por favor, intenta ingresar un correo válido.")
         
 strings_no_aceptados = ["usuario@ejemplo", "nombre.apellido@ejemplo", 
                         "@ejemplo.com", "usuario@"]                         #Realizamos una lista de strings que no deberian ser aceptados.
@@ -69,4 +69,4 @@ for strings in strings_no_aceptados:
         print(f"'{strings}' es una direccion de correo válida!!")
     else:                                                                   
         print(f"'{strings}' no es una direccion de correo válida!!")
-        
+        print("Por favor, intenta ingresar un correo válido.")'''
